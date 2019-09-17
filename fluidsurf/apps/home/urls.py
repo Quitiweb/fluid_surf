53dfdea118428c7bb49c4fd8cf302f3d4ef4e1fc
unknown <jlramos97@gmail.com>
2019-09-17 16:49:09 +0200

    FS-14 Añadido menu hamburguesa con los links de las 3 paginas actuales

--- fluidsurf/apps/home/urls.py
+++ fluidsurf/apps/home/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/', views.index, name='index'),
     path('mensaje-enviado', views.mensaje_enviado, name='mensaje-enviado'),
     path('formulario', views.formulario, name='formulario'),
     path('solicitud-recibida', views.solicitud_recibida, name='solicitud-recibida'),
