f9ca5ddde8559b4c3df9aee4c63dc768e65026ed
Jose Luis <joseluis@quitiweb.com>
2019-10-16 13:07:50 +0200

    FS-59 Restringida la posibilidad de elegir Admin en el formulario de registro

--- fluidsurf/apps/users/forms.py
+++ fluidsurf/apps/users/forms.py
@@ -3,11 +3,20 @@ from django import forms
 # from localflavor.es.forms import ESIdentityCardNumberField
 
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import ugettext_lazy as _
 
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+
+    CHOICES = (
+        ('SURFERO', _('Surfer')),
+        ('FOTOGRAFO', _('Photographer')),
+    )
+
+    tipo_de_usuario = forms.ChoiceField(choices=CHOICES)
+
     class Meta(UserCreationForm):
         model = CustomUser
         fields = ('tipo_de_usuario', 'username', 'email')
