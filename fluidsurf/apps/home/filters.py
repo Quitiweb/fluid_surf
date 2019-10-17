540832aa81738d2bdb8f3be2cea117fca21e8869
Jose Luis <joseluis@quitiweb.com>
2019-10-17 10:49:58 +0200

    Comienza a funcionar el filtro de productos por nombre y nombre de fotografo

--- fluidsurf/apps/home/filters.py
+++ fluidsurf/apps/home/filters.py
@@ -1,9 +1,14 @@
 import django_filters
+from django.forms import TextInput
+from django.utils.translation import ugettext_lazy as _
 
 from fluidsurf.apps.home.models import Producto
 
+
 class ProductoFilter(django_filters.FilterSet):
-    nombre = django_filters.CharFilter(lookup_expr='icontains')
+    nombre = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': _('Name')}))
+    user__username = django_filters.CharFilter(widget=TextInput(attrs={'placeholder': _('Photographer')}))
+
     class Meta:
         model = Producto
-        fields = ['nombre']
+        fields = ['nombre', 'user__username']
