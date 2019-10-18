ef714b62d14df0b3fefd66b0abeabf62c3e17c3e
Jose Luis <joseluis@quitiweb.com>
2019-10-18 16:16:23 +0200

    Arreglados un par de estilos

--- static/main/assets/js/main.js
+++ static/main/assets/js/main.js
@@ -32,7 +32,7 @@
 				keyboardShortcuts: {
 
 					// If true, enables scrolling via keyboard shortcuts.
-						enabled: true,
+						enabled: false,
 
 					// Sets the distance to scroll when using the left/right arrow keys.
 						distance: 50
@@ -54,7 +54,7 @@
 				scrollZones: {
 
 					// If true, enables scrolling via scroll zones on the left/right edges of the scren.
-						enabled: true,
+						enabled: false,
 
 					// Sets the speed at which the page scrolls when a scroll zone is active (higher = faster scroll, lower = slower scroll).
 						speed: 15
