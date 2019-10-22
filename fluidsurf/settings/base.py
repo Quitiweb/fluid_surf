f18cb3557a9ba0fa40813088f15685a480c59ff4
Quitiweb <rafa@quitiweb.com>
2019-10-22 18:30:34 +0200

    Con la anterior prueba, no cargan los estilos del nuevo admin. Vamos a probar con esto.

--- fluidsurf/settings/base.py
+++ fluidsurf/settings/base.py
@@ -176,8 +176,8 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    # 'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    # 'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
 # Allauth access
