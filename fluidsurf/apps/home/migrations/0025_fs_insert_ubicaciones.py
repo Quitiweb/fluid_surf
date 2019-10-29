8a31b6cd3d4eba6aeb4cea5835d90625db61d0bd
Jose Luis <joseluis@quitiweb.com>
2019-10-29 12:15:18 +0100

    FS-60 Modificada la migracion porque daba un problema en el where not exists

--- fluidsurf/apps/home/migrations/0025_fs_insert_ubicaciones.py
+++ fluidsurf/apps/home/migrations/0025_fs_insert_ubicaciones.py
@@ -8,16 +8,10 @@ class Migration(migrations.Migration):
     ]
     operations = [
 
-        migrations.RunSQL( "insert into home_ubicacion(spot, geoloc) values ('Europe', '36.557223,-6.303212')" +
-                            "where not exists(select 1 from home_ubicacion where spot = 'Europe');"),
-        migrations.RunSQL( "insert into home_ubicacion(spot, geoloc) values ('Oceania', '-33.865143,151.2099')" +
-                            "where not exists(select 1 from home_ubicacion where spot = 'Oceania');"),
-        migrations.RunSQL( "insert into home_ubicacion(spot, geoloc) values ('Asia', '34.295833,132.319722')" +
-                            "where not exists(select 1 from home_ubicacion where spot = 'Asia');"),
-        migrations.RunSQL( "insert into home_ubicacion(spot, geoloc) values ('Africa', '51.2099,33.865143')" +
-                            "where not exists(select 1 from home_ubicacion where spot = 'Africa');"),
-        migrations.RunSQL( "insert into home_ubicacion(spot, geoloc) values ('North America', '34.42083,-119.69819')" +
-                            "where not exists(select 1 from home_ubicacion where spot = 'North America');"),
-        migrations.RunSQL( "insert into home_ubicacion(spot, geoloc) values ('South America', '-33.03553,-71.64182')" +
-                            "where not exists(select 1 from home_ubicacion where spot = 'South America');")
+        migrations.RunSQL( "insert into home_ubicacion(spot, geoloc) values ('Europe', '36.557223,-6.303212')"),
+        migrations.RunSQL( "insert into home_ubicacion(spot, geoloc) values ('Oceania', '-33.865143,151.2099')"),
+        migrations.RunSQL( "insert into home_ubicacion(spot, geoloc) values ('Asia', '34.295833,132.319722')"),
+        migrations.RunSQL( "insert into home_ubicacion(spot, geoloc) values ('Africa', '51.2099,33.865143')"),
+        migrations.RunSQL( "insert into home_ubicacion(spot, geoloc) values ('North America', '34.42083,-119.69819')"),
+        migrations.RunSQL( "insert into home_ubicacion(spot, geoloc) values ('South America', '-33.03553,-71.64182')"),
     ]
