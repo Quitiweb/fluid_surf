982c8b0e86105f0f440fc07bbdc635b879e1e97d
Jose Luis <joseluis@quitiweb.com>
2019-10-30 10:26:08 +0100

    FS-106 Actualizados los filtros

--- fluidsurf/apps/home/filters.py
+++ fluidsurf/apps/home/filters.py
@@ -8,8 +8,8 @@ from fluidsurf.apps.home.models import Producto
 class ProductoFilter(django_filters.FilterSet):
     nombre = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': _('Name')}))
     spot = django_filters.CharFilter(lookup_expr='icontains', widget=TextInput(attrs={'placeholder': _('Spot')}))
-    user__username = django_filters.CharFilter(widget=TextInput(attrs={'placeholder': _('Photographer')}))
+    user__alias = django_filters.CharFilter(widget=TextInput(attrs={'placeholder': _('Photographer')}))
 
     class Meta:
         model = Producto
-        fields = ['nombre', 'user__username', 'spot']
+        fields = ['nombre', 'user__alias', 'spot']
