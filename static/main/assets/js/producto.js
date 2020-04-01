e43fd6d49b0dab17775f29c884ba99a84bd32fec
qw-jl <joseluis@quitiweb.com>
2020-04-01 18:50:01 +0200

    FS-178 Modificada la plantilla de producto para que el carrusel se vea bien

--- static/main/assets/js/producto.js
+++ static/main/assets/js/producto.js
@@ -5,21 +5,5 @@ $(function () {
             $(this).attr('href', link);
         });
 
-
-        let inicio = 0;
-        $.each($('.for2par'), function () {
-            inicio += 2;
-
-            let link = $(this).find('img#' + (inicio)).attr('src');
-            $(this).attr('href', link);
-        });
-
-        let inicioImpar = -1;
-        $.each($('.for2impar'), function () {
-                inicioImpar += 2;
-
-                let link = $(this).find('img#' + (inicioImpar)).attr('src');
-                $(this).attr('href', link);
-        });
     });
 });
