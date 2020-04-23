a97908761853112db2875635e71f538935b86680
qw-jl <joseluis@quitiweb.com>
2020-04-23 13:05:52 +0200

    FS-180 Filtros para todas las capas de la plataforma

--- static/main/assets/js/subir-producto.js
+++ static/main/assets/js/subir-producto.js
@@ -183,7 +183,7 @@ function refreshQS() {
     }
 }
 
-function loadQS(contValue= null, paisValue = null) {
+function loadQS(contValue= null, paisValue = null, areaValue = null, spotValue = null) {
     for (spot of spotQS) {
         continentes.push(spot['continente']);
         if (spot['pais']) { paises.push(spot['pais']); }
@@ -205,7 +205,6 @@ function loadQS(contValue= null, paisValue = null) {
         if (contValue) {
             document.getElementById('selectCont').value=contValue
         }
-
     }
 
     for (pais of paises) {
@@ -217,7 +216,6 @@ function loadQS(contValue= null, paisValue = null) {
         if (paisValue) {
             document.getElementById('selectPais').value=paisValue
         }
-
     }
 
     for (area of areas) {
@@ -225,6 +223,10 @@ function loadQS(contValue= null, paisValue = null) {
         opt.value = area;
         opt.innerHTML= area;
         document.getElementById('selectArea').appendChild(opt)
+
+        if (areaValue) {
+            document.getElementById('selectArea').value=areaValue
+        }
     }
 
     for (spot of spots) {
@@ -232,10 +234,15 @@ function loadQS(contValue= null, paisValue = null) {
         opt.value = spot;
         opt.innerHTML= spot;
         document.getElementById('selectSpot').appendChild(opt)
+
+        if (spotValue) {
+            document.getElementById('selectSpot').value=spotValue
+        }
     }
 }
 
 function refreshContinente(value) {
+    refreshQS();
 
     // for para obtener un spot aleatorio con ese pais
     for(spot of spotQS) {
@@ -245,15 +252,23 @@ function refreshContinente(value) {
     }
 
     spotQS = spotQS.filter(function(el) {
-        return el['continente'] === paisEx['continente']
+        return el['continente'] === contEx['continente']
     });
 
+    $('select').empty()
 
-    loadQS();
+    for(spot of spotQS) {
+        if(spot['continente'] !== contEx['continente']) {
+            delete spot['pais'];
+            delete spot['area'];
+            delete spot['spot'];
+        }
+    }
+
+    loadQS(contEx['continente']);
 }
 
 function refreshPais(value) {
-
     refreshQS();
 
      var paisEx;
@@ -277,8 +292,59 @@ function refreshPais(value) {
             delete spot['spot'];
         }
     }
-
     loadQS(paisEx['continente'], paisEx['pais']);
+}
+
+function refreshArea(value) {
+     refreshQS();
+
+     var areaEx;
+     // for para obtener un spot aleatorio con ese pais
+    for(spot of spotQS) {
+        if(spot['area'] == value) {
+            areaEx = spot;
+        }
+    }
+
+   spotQS = spotQS.filter(function(el) {
+        return el['pais'] === areaEx['pais']
+            || el['continente'] === areaEx['continente']
+            || el['area'] === areaEx['area']
+    });
+
+    $('select').empty()
+
+    for(spot of spotQS) {
+        if(spot['area'] !== areaEx['area']) {
+            delete spot['spot'];
+        }
+    }
+    loadQS(areaEx['continente'], areaEx['pais'], areaEx['area']);
+
+}
+
+function refreshSpot(value) {
+     refreshQS();
+
+     var spotEx;
+     // for para obtener un spot aleatorio con ese pais
+    for(spot of spotQS) {
+        if(spot['spot'] == value) {
+            spotEx = spot;
+        }
+    }
+
+   spotQS = spotQS.filter(function(el) {
+        return el['pais'] === spotEx['pais']
+            || el['continente'] === spotEx['continente']
+            || el['area'] === spotEx['area']
+            || el['spot'] === spotEx['spot']
+    });
+
+    $('select').empty()
+
+    loadQS(spotEx['continente'], spotEx['pais'], spotEx['area'], spotEx['spot']);
+
 }
 
 /**
@@ -302,6 +368,17 @@ function selectElement(id, valueToSelect) {
     element.value = valueToSelect;
 }
 
+document.getElementById('selectCont').onchange = (e) => {
+    $.ajax({
+        url: "subir-producto",
+    }).success(function(response) {
+        paises = [];
+        areas = [];
+        spots = [];
+        refreshContinente(e.target.value);
+    });
+}
+
 document.getElementById('selectPais').onchange = (e) => {
     $.ajax({
         url: "subir-producto",
@@ -312,7 +389,25 @@ document.getElementById('selectPais').onchange = (e) => {
         refreshPais(e.target.value);
     });
 }
-// // TODO Cambio de continente
-// document.getElementById('selectCont').onchange = (e) => {
-//
-// }
+
+document.getElementById('selectArea').onchange = (e) => {
+    $.ajax({
+        url: "subir-producto",
+    }).success(function(response) {
+        paises = [];
+        areas = [];
+        spots = [];
+        refreshArea(e.target.value);
+    });
+}
+
+document.getElementById('selectSpot').onchange = (e) => {
+    $.ajax({
+        url: "subir-producto",
+    }).success(function(response) {
+        paises = [];
+        areas = [];
+        spots = [];
+        refreshSpot(e.target.value);
+    });
+}
