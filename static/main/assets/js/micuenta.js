9ec8163a5497dd8e0c310c50ea83158becab9cc1
qw-jl <joseluis@quitiweb.com>
2020-06-04 09:52:54 +0200

    FS-180 Guardado en localStorage el continente y el pais una vez se guarda la info de mi-cuenta

--- static/main/assets/js/micuenta.js
+++ static/main/assets/js/micuenta.js
@@ -2,3 +2,20 @@ $(document).ready(function () {
     initSpots();
 });
 
+$('#guardar').on("click", function(e){
+    e.preventDefault();
+
+    var contDftl = document.getElementById('selectCont').value;
+    var paisDftl = document.getElementById('selectPais').value;
+
+    var text = '{' +
+                  '"cont":' + '"' + contDftl + '"' +
+                ', "pais": ' + '"' + paisDftl + '"' +
+                '}'
+
+    localStorage.setItem('localCountry', text);
+
+    console.log(localStorage.getItem('localCountry').cont)
+
+    $("#form").submit();
+});
